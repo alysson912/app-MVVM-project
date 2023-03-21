83f9829b91ceff11ae1cc9e2a82082706ce35393
Alysson Menezes <almenezes912@gmail.com>
2023-03-21 13:55:28 -0300

    projeto inicial criado e configurado para viewCode

---
+++ app-MVVM/app-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  app-MVVM
+//
+//  Created by ALYSSON DODO on 21/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
