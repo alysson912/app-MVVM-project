6a864cda3b75289ea0f425f82feb68f50a7ed833
Alysson Menezes <almenezes912@gmail.com>
2023-04-11 14:23:49 -0300

    finalizando Mock

--- app-MVVM/app-MVVM/src/Home/ViewModel/HomeViewModel.swift
+++ app-MVVM/app-MVVM/src/Home/ViewModel/HomeViewModel.swift
@@ -9,15 +9,15 @@ import UIKit
 
 class HomeViewModel {
     
-    private var posts: [Posts] = []
+    private var service = HomeService()
+    private var posts = [Posts]()
+    private var story = [Stories]()
     
-    public var getListPosts: [Posts]{ // acessando objeto atravez de uma variave computada
+    public var getListPosts: [Posts]{ // acessando objeto atraves de uma variavel computada
         posts
     }
     
-    private var story: [Stories]  = []
-    
-    public var getListStory: [Stories]{ // acessando objeto atravez de uma variave computada
+    public var getListStory: [Stories]{ // acessando objeto atraves de uma variave computada
         story
     }
     
@@ -29,6 +29,14 @@ class HomeViewModel {
             return  CGSize(width: 120, height: frame.height)// se primeira cell tenha 120 mantenha essa proporção
         }else{
             return CGSize(width: frame.width - 120, height: frame.height) // caso contrario pegue a largura e diminua 120
+        }
+    }
+    public func fetchAllRequest(){
+        service.getHomeDataJson { HomeData, error in
+            if error == nil {
+                self.posts = HomeData?.posts ?? []
+                self.story = HomeData?.stories ?? []
+            }
         }
     }
 }
